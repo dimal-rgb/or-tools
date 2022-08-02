0f58f66471f962d4dcd2a8afbd9c9df8a4c8dff7
Dima Levikov <dimal@hai-110-lap.qb.hailotech>
2022-08-02 16:08:10 +0300

    wip

--- makefiles/Makefile.cpp.mk
+++ makefiles/Makefile.cpp.mk
@@ -22,7 +22,7 @@ endif
 OR_TOOLS_LIBS = $(LIB_DIR)/$(LIB_PREFIX)ortools.$L
 OR_TOOLS_AR = $(LIB_DIR)/$(LIB_PREFIX)ortools.a
 OR_TOOLS_LNK += $(PRE_LIB)ortools$(POST_LIB)
-LIB_JEMALLOC_DIR = /usr/local/lib
+LIB_JEMALLOC_DIR = external/jemalloc/lib
 
 HAS_CCC = true
 ifndef CCC
